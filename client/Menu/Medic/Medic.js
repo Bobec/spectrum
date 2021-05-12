865b5fcb222e468afefeffab420889a8dfc1bc0a
Bobec <bogdanciurezu98@gmail.com>
2021-05-12 11:09:30 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ client/Menu/Medic/Medic.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Getter from '../../Utils/Getter';
+import config from '../../Utils/config';
+import { Actions } from 'react-native-router-flux';
+import { StyleSheet, Text, Button, View, Alert } from 'react-native';
+// TODO: Menu medic.
+const MESSAGE_PARENT = "Copilul dumneavoastră vă solicită asistența! \n Mulțumim frumos, \n Echipa SPECTRUM ";
+
+export default class Medic extends Component {
+
+  
+
+  callParent = async () => {
+    let get = new Getter();
+
+    let parentPhoneNo = await get.getPhoneNumber();
+    console.log(parentPhoneNo);
+    let response = await fetch(config.API_ADDR + '/call', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        to: parentPhoneNo,
+        body: MESSAGE_PARENT
+      })
+    });
+    let res = await response.json();  
+    console.log(res);
+  }
+
+  render() {
+    return(
+      <View> 
+          <Button title="suna un parinte" key="callParent" onPress={() => this.callParent()}/>
+          <Button title="modul doctor" key="modulDoctor" onPress={() => Actions.assistance()}/>
+      </View>
+    )
+  }
+}
---
+++ client/Menu/Recognition/Recognition.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, Button, View, Alert } from 'react-native';
+// TODO: Take the angular project and adapt it to this one
+
+export default class Recognition extends Component {
+
+  render() {
+    return(
+        <div>
+            Recognition          
+        </div>
+    )
+  }
+}
