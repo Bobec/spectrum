80a0a305ba6fffae79cbed1277528687d1a60869
Bobec <bogdanciurezu98@gmail.com>
2021-05-13 16:35:23 +0300

    test deploy24

--- pages/api/server.js
+++ pages/api/server.js
@@ -1,8 +1,17 @@
-// const faceapi = require("face-api.js")  
-// const canvas = require("canvas")  
-// const fs = require("fs")  
-// const path = require("path")
-// const fetch = require('node-fetch');
+const faceapi = require("face-api.js")  
+const canvas = require("canvas")  
+const fs = require("fs")  
+const path = require("path")
+const fetch = require('node-fetch');
+const tf = require('@tensorflow/tfjs');
+// const { serverRuntimeConfig } = getConfig()
+
+
+// mokey pathing the faceapi canvas
+const { Canvas, Image, ImageData } = canvas  
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
+faceapi.env.monkeyPatch({ fetch: fetch });
+
 
 
 
@@ -28,15 +37,14 @@
 //     fs.writeFileSync(path.resolve(baseDir, fileName), buf)
 //   }
 
-import  path  from "path"
-import faceapi from "face-api.js" 
+// import path from "path"
+// const faceapi = require("face-api.js") 
+// import getConfig from 'next/config'
 // const { serverRuntimeConfig } = getConfig()
-import fetch from 'node-fetch'
+// const canvas = require("canvas")  
+// const fetch = require('node-fetch');
 
-// mokey pathing the faceapi canvas
-// const { Canvas, Image, ImageData } = canvas  
-// faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
-// faceapi.env.monkeyPatch({ fetch: fetch });
+const faceDetectionNet = faceapi.nets.ssdMobilenetv1
 
 // SsdMobilenetv1Options
 const minConfidence = 0.5
@@ -49,30 +57,29 @@ const scoreThreshold = 0.5
 const minFaceSize = 50  
 const scaleFactor = 0.8
 
+
 export default async (req, res) => {
     res.statusCode = 200
 
     // load weights
-    const MODELS_URL = '/static/models'
+    const MODELS_URL ='./static/models';
     
-    // await faceapi.nets.faceLandmark68Net  .loadFromDisk   (MODELS_URL)
-    // await faceapi.nets.faceRecognitionNet .loadFromDisk   (MODELS_URL)
-    // await faceapi.nets.faceExpressionNet  .loadFromDisk   (MODELS_URL)
-    // await faceapi.nets.ssdMobilenetv1     .loadFromDisk   (MODELS_URL)
+    await faceapi.nets.faceLandmark68Net  .loadFromDisk   (MODELS_URL)
+    await faceapi.nets.faceRecognitionNet .loadFromDisk   (MODELS_URL)
+    await faceapi.nets.faceExpressionNet  .loadFromDisk   (MODELS_URL)
+    await faceapi.nets.ssdMobilenetv1     .loadFromDisk   (MODELS_URL)
 
     // // // // load the image
-    // const img = await canvas.loadImage('./static/imgs_src/da.jpeg')
-    // // // // // create a new canvas and draw the detection and landmarks
-    //  const out = await faceapi.createCanvasFromMedia(img)
-    // // // faceapi.drawLandmarks(out, results.map(res => res.landmarks), { drawLines: true, color: 'red' })
-    // const detectionsWithExpressions = await faceapi.detectAllFaces(img).withFaceLandmarks().withFaceExpressions()
+    const img = await canvas.loadImage('./static/imgs_src/da.jpeg')
+
+    const detectionsWithExpressions = await faceapi.detectAllFaces(img).withFaceLandmarks().withFaceExpressions()
 
     // // // // // // save the new canvas as image
     // // // //  console.log(detectionsWithExpressions[0].expressions)
     // // // // // saveFile('faceLandmarkDetection.jpg', out.toBuffer('image/jpeg'))
     // // // // // console.log('done, saved results to out/faceLandmarkDetection.jpg')
     
-    // res.json(detectionsWithExpressions[0].expressions)
-    res.json({"hello": MODELS_URL})
+    res.json(detectionsWithExpressions[0].expressions)
+    // res.json({"hello": MODELS_URL})
     res.end()
 }
