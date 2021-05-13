5dbbde6ae407fe03130b38061a09665a3ceab655
Bobec <bogdanciurezu98@gmail.com>
2021-05-13 15:16:22 +0300

    test imported4

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,5 +1,5 @@
-import { faceapi } from "face-api.js"
-import { canvas } from "canvas"
+import  faceapi  from "face-api.js"
+import  canvas  from "canvas"
 export default async (req, res) => {
     res.statusCode = 200
     const MODELS_URL = '/static/models'
